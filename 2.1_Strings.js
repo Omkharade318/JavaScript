af652181c1abf998a063aa15137b7f2a6f697996
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2024-01-21 22:28:19 +0530

    Add files via upload

---
+++ 2.1_Strings.js
@@ -0,0 +1,25 @@
+// Strings
+
+let str1 = "King of the pirates";
+let str2 = new String("Luffy")
+console.log(str1 + " " + str2);
+
+console.log(" ")
+// Template literals
+let b = `Pratik`  // `` These are called backtics (you'll generally find them above tab)
+let a = `Nikhil` // These can even be in " "  or ''
+
+let c = `${a} is a friend of ${b}`
+console.log(c);
+
+console.log('');
+
+// lenth property of strings
+let x = str1.length;
+let y = str2.length;
+console.log(`The lenth of str1 is: ${x} and the lenth of str2 is: ${y}`);
+
+let int1 = "Luffy"
+console.log(int1.toUpperCase());
+console.log(int1.toLocaleLowerCase());
+
